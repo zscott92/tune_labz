9d164fa041faedc95f04c2238ec8cebfe2f0b008
MaxPower <mxmcoursey@gmail.com>
2019-09-20 13:08:38 -0400

    updated framework

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 // Add routes, both API and view
 app.use(routes);
 app.use(express.static("./client/public"));
