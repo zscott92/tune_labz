bee0625b8b2e8f2cd0ce173ea752bbf80cdedec9
Brett Scottini <49254928+bscottini1@users.noreply.github.com>
2019-09-23 14:08:23 -0400

    Revert "Sidebar"

--- client/src/components/Songtree/songtree.js
+++ client/src/components/Songtree/songtree.js
@@ -1,38 +1,19 @@
 
 const API = require("../utils/API");
-const { Vizualizer } = require("../TrackVisualizer/visualizer");
+const {Vizualizer} = require("../TrackVisualizer/visualizer");
 const songs = require("../../../../models");
-import TreeMenu from 'react-simple-tree-menu'
 
 
 class SongTree extends Component {
     render() {
         return (
-            // Use the default minimal UI
-            <TreeMenu data={treeData} />
-
-            // Use any third-party UI framework
-            <TreeViewMenu
-                data={treeData}
-                onClickItem={({ key, label, ...props }) => {
-                    this.navigate(props.url); // user defined prop
-                }}
-                debounceTime={125}>
-                {({ search, items }) => (
-                    <>
-                        <Input onChange={e => search(e.target.value)} placeholder="Type and search" />
-                        <ListGroup>
-                            {items.map(props => (
-                                // You might need to wrap the third-party component to consume the props
-                                // check the story as an example
-                                // https://github.com/iannbing/react-simple-tree-menu/blob/master/stories/index.stories.js
-                                <ListItem {...props} />
-                            ))}
-                        </ListGroup>
-                    </>
-                )}
-            </TreeViewMenu>
-
+            <div className="List">
+                <div className="List-header">
+                    {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                    <h2>Song Dependencies</h2>
+                </div>
+                <Tree nodes={data} />
+            </div>
         );
     }
 }
