2ead7df5ca449c424052832a2ca6b55476f75d21
Lawrence Gober <goberlawrence@icloud.com>
2019-09-24 13:05:32 -0400

    Routing on two pages functionting / compiling succesfully

---
+++ client/src/components/SongData/SongData.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function SongData () {
+    return (
+        <div>
+            <div className="songinformation">
+                <h1>Track Name</h1>
+                <h2>Artist</h2>
+                <h2>Song Here</h2>
+            </div>
+        </div>
+    );
+}
+
+export default SongData;
