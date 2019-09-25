87a19589f7304d4364a171011717c823e562eabc
zscott92 <38567584+zscott92@users.noreply.github.com>
2019-09-25 00:22:03 -0400

    components work in sandox environments

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,20 +3,21 @@ import Tree from "../components/Tree/Tree";
 import SongData from "../components/SongData/SongData";
 
 
-    <div>
-      <div className="Dashboard text-center">
-        <div className="row">
-          <div className="col-4 sidebar">
-            <Tree />
-          </div>
-          <div className="col-8 songdata">
-            <SongData />
-                </div>
-            <Upload />
+function Dashboard() {
+  <div>
+    <div className="Dashboard text-center">
+      <div className="row">
+        <div className="col-4 sidebar">
+          <Tree />
         </div>
+        <div className="col-8 songdata">
+          <SongData />
+        </div>
+        <Upload />
       </div>
     </div>
-
+  </div>
+}
 
   //When calling tree , associate with a specific user id! FOR USER SPECIFIC SONGS!!!!!!
 
