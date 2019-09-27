2c4fff0ca2989079d494e68463421b784c7762dc
Tphorton14 <Tphorton14@gmail.com>
2019-09-27 11:16:47 -0400

    keep user login after refresh

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
 import { GoogleLogin } from 'react-google-login';
 import config from '../config.json';
+// import { storage } from "googleapis/build/src/apis/storage";
 
 class Login extends Component {
 
     state = { isAuthenticated: false, user: null, token: '' }
+
+    componentDidMount = () => {
+        let accessToken = localStorage.getItem('token')
+        console.log(accessToken);
+        const tokenBlob = new Blob([JSON.stringify({ access_token: accessToken }, null, 2)], { type: 'application/json' });
+        const options = {
+            method: 'POST',
+            body: tokenBlob,
+            mode: 'cors',
+            cache: 'default'
+        };
+        fetch('/auth/google', options).then(r => {
+           
+            const token = r.headers.get('x-auth-token');
+            r.json().then(user => {
+                if (token) {
+                    this.setState({ isAuthenticated: true, user, token });
+                                        // key value pair
+                    localStorage.setItem('token', token);
+                }
+            });
+        })
+
+    }
 
     logout = () => {
         this.setState({ isAuthenticated: false, token: '', user: null })
@@ -15,8 +40,10 @@ class Login extends Component {
     };
 
     googleResponse = (response) => {
-        console.log("test", response)
+        // console.log("test", response)
         const tokenBlob = new Blob([JSON.stringify({ access_token: response.accessToken }, null, 2)], { type: 'application/json' });
+
+        localStorage.setItem('token', response.accessToken);
         const options = {
             method: 'POST',
             body: tokenBlob,
@@ -27,7 +54,11 @@ class Login extends Component {
             const token = r.headers.get('x-auth-token');
             r.json().then(user => {
                 if (token) {
-                    this.setState({ isAuthenticated: true, user, token })
+                    console.log(token);
+                    this.setState({ isAuthenticated: true, user, token });
+                                        
+                    
+                    
                 }
             });
         })
